6d35c64168e497a244162e6021622fd546105055
Your Name <you@example.com>
2025-11-09 03:23:11 -0300

    integracao mysql com api em produtos1

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { ProdutoService } from './produto/produto.services';
 import { ProdutoController } from './produto/produto.controller';
 import { ProdutoEntityRepository } from './produto/produto.repository';
 import { DatabaseModule } from './database/database.module';
+import { UsuarioService } from './usuario/usuario.service';
+import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, UsuarioModule],
   controllers: [ProdutoController],
   providers: [ProdutoService, ProdutoEntityRepository],
   exports: [ProdutoService],
