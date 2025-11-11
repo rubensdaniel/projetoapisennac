f5366fd94a50de66e3dab91bf323d55440a7341b
Rubens Daniel <rubensmartirano@gmail.com>
2025-11-10 22:13:33 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.enableCors();
+
+  
   await app.listen(process.env.PORT ? +process.env.PORT : 3000);
 }
 bootstrap();
